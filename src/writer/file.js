cec71990893cf3d0117703e9b29122654c7bff33
M.C. van den Boogaart <scola@kvdb.name>
2019-10-24 13:19:20 +0200

    Fix defaults in file writer

--- src/writer/file.js
+++ src/writer/file.js
@@ -67,11 +67,11 @@ export default class FileWriter extends Worker {
     const files = [];
 
     for (let i = 0; i < input.length; i += 1) {
-      defaults({
+      defaults(input[i], {
         date,
         path: this._base + '/' + this._path +
           date + '-' + shortid.generate()
-      }, input[i]);
+      });
 
       if (input[i].type.match(/^image\//) && this._resize.length > 0) {
         this._prepareResize(files, input[i]);
